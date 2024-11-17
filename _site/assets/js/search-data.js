c9290fdb1dd7c7f4d2f554579510765ad82cf461
liacov <lauraiacovissi@gmail.com>
2024-11-17 19:08:47 +0100

    erased ignored files

--- _site/assets/js/search-data.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Tübingen Pre-NeurIPS Fest Upcoming",
-        "excerpt":"Dear all, the end of the year 2024 is coming closer! We would like to celebrate the approaching end of this academic year with the second edition of the Tübingen Pre-NeurIPS Fest, a poster session followed by a festive get-together: ","categories": [],
-        "tags": ["event"],
-        "url": "http://localhost:4000/poster-event/"
-      }]
